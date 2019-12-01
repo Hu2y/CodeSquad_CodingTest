9ff59121778df7b4aa1c22dcf65f216a4fc8733f
Hu2y <sanghun_@daum.net>
2019-12-01 17:21:03 +0900

    * 게임 시작 메시지 구현
    1단계 요구사항 (1) 완료
     - 게임이 시작되면 "첫 번째 타자가 타석에 입장했습니다." 메시지와 함께 경기를 진행한다.

---
+++ src/components/Step-1.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Step-1</h1>
+    <button @click="startBtn">게임시작</button>
+    <button @click="nextBtn" :disabled="gameResult">진행</button>
+    <div v-show="startView">
+      <div v-show="firstHitter">첫 번째 타자가 타석에 입장 했습니다.</div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      gameResult: true, // 진행 버튼 활성화
+      startView: false,
+      firstHitter : true, // 게임 시작 메시지 
+    }
+  },
+  methods: {
+    startBtn() {
+      this.startView = true
+      this.gameResult = false
+    },
+    nextBtn() {
+      this.firstHitter = false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
